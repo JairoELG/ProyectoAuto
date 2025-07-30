7d1446b1575af38d8a3296af4b5576a3519753e0
PauAlvarez <pamolinaal@est.utn.ac.cr>
2025-07-30 17:57:54 -0600

    clase luces traseras
    se crea la clase luces traseras con sus getters y setters, metodo constructor y metodos necesaerios

---
+++ src/LucesFreno/LucesTraseras.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package LucesFreno;
+
+/**
+ *
+ * @author HP I3
+ */
+public class LucesTraseras {
+  
+    private boolean encendidas;
+    private boolean frenoActivado;
+    private int intensidad; // de 0 a 100 (opcional)
+    
+    // Getters y Setters
+    public boolean isEncendidas() {
+        return encendidas;
+    }
+
+    public void setEncendidas(boolean encendidas) {
+        this.encendidas = encendidas;
+    }
+
+    public boolean isFrenoActivado() {
+        return frenoActivado;
+    }
+
+    public void setFrenoActivado(boolean frenoActivado) {
+        this.frenoActivado = frenoActivado;
+    }
+
+    public int getIntensidad() {
+        return intensidad;
+    }
+
+    public void setIntensidad(int intensidad) {
+        this.intensidad = intensidad;
+    }
+
+    // Constructor
+    public LucesTraseras() {
+        this.encendidas = false;
+        this.frenoActivado = false;
+        this.intensidad = 50; // valor predeterminado
+    }
+
+    // Encender luces traseras
+    public void encender() {
+        this.encendidas = true;
+    }
+
+    // Apagar luces traseras
+    public void apagar() {
+        this.encendidas = false;
+        this.frenoActivado = false;
+    }
+
+    // Activar freno (enciende más fuerte)
+    public void activarFreno() {
+        this.frenoActivado = true;
+        this.intensidad = 100;
+    }
+
+    // Desactivar freno
+    public void desactivarFreno() {
+        this.frenoActivado = false;
+        this.intensidad = 50;
+    }
+
+    
+}
+
+
