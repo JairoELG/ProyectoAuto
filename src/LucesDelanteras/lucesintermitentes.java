e00b277e204e87f12c56bfbf58895b2c7b63f98a
PauAlvarez <pamolinaal@est.utn.ac.cr>
2025-07-30 17:44:08 -0600

    clase luces intermitentes
    se crea la clase luces intermitentes con sus getters y setters, metodo constructor y metodos necesarios

---
+++ src/LucesDelanteras/lucesintermitentes.java
@@ -0,0 +1,79 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package LucesDelanteras;
+
+/**
+ *
+ * @author HP I3
+ */
+public class lucesintermitentes {
+ 
+    private boolean activadas;        // Si están activadas o no
+    private boolean estadoActual;     // Si están encendidas o apagadas
+    private long intervalo;           // Tiempo de parpadeo en milisegundos
+
+   // Getter y Setter de activadas
+    public boolean isActivadas() {
+        return activadas;
+    }
+
+    public void setActivadas(boolean activadas) {
+        this.activadas = activadas;
+    }
+
+    // Getter y Setter de estadoActual
+    public boolean isEstadoActual() {
+        return estadoActual;
+    }
+
+    public void setEstadoActual(boolean estadoActual) {
+        this.estadoActual = estadoActual;
+    }
+
+    // Getter y Setter de intervalo
+    public long getIntervalo() {
+        return intervalo;
+    }
+
+    public void setIntervalo(long intervalo) {
+        this.intervalo = intervalo;
+    }
+
+   
+     // Constructor
+    public lucesintermitentes(long intervalo) {
+        this.activadas = false;
+        this.estadoActual = false;
+        this.intervalo = intervalo;
+    }
+
+    
+    
+     // Método para activar las luces intermitentes
+    public void activar() {
+        this.activadas = true;
+    }
+
+    // Método para desactivar las luces intermitentes
+    public void desactivar() {
+        this.activadas = false;
+        this.estadoActual = false; // se apagan cuando se desactivan
+    }
+
+    // Alternar el estado (encendidas/apagadas)
+    public void alternarEstado() {
+        if (activadas) {
+            this.estadoActual = !this.estadoActual;
+        } else {
+            this.estadoActual = false;
+        }
+    }
+}
+
+ 
+
+
+
+
